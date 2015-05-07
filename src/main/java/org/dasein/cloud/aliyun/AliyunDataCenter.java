511a0e10047fd22aa8a8563e1433a753b6fc56e9
Jeffrey Yan <jeffrey.yan@software.dell.com>
2015-05-07 14:57:04 +0800

    finish DC and revise AliyunMethod

--- src/main/java/org/dasein/cloud/aliyun/AliyunDataCenter.java
+++ src/main/java/org/dasein/cloud/aliyun/AliyunDataCenter.java
@@ -24,15 +24,26 @@ package org.dasein.cloud.aliyun;
 import org.apache.log4j.Logger;
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
-import org.dasein.cloud.dc.*;
+import org.dasein.cloud.ProviderContext;
+import org.dasein.cloud.dc.AbstractDataCenterServices;
+import org.dasein.cloud.dc.DataCenter;
+import org.dasein.cloud.dc.DataCenterCapabilities;
+import org.dasein.cloud.dc.DataCenterServices;
+import org.dasein.cloud.dc.Region;
+import org.dasein.cloud.util.APITrace;
+import org.dasein.cloud.util.Cache;
+import org.dasein.cloud.util.CacheLevel;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -61,43 +72,130 @@ public class AliyunDataCenter extends AbstractDataCenterServices<Aliyun> impleme
     }
 
     @Override
-    public @Nonnull Iterable<DataCenter> listDataCenters(@Nonnull String providerRegionId) throws InternalException, CloudException {
-        Map<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("RegionId", providerRegionId);
-        AliyunMethod method = new AliyunMethod(getProvider(), AliyunMethod.Category.ECS, "DescribeZones", parameters);
+    public @Nullable DataCenter getDataCenter(@Nonnull String providerDataCenterId) throws InternalException, CloudException {
+        ProviderContext context = getProvider().getContext();
+        if (context == null) {
+            throw new InternalException("No context exists for this request");
+        }
 
-        JSONObject json = method.get().asJson();
-        List<DataCenter> result = new ArrayList<DataCenter>();
-        try {
-            JSONArray regions = json.getJSONObject("Zones").getJSONArray("Zone");
-            for (int i = 0; i < regions.length(); i++) {
-                JSONObject region = regions.getJSONObject(i);
-                String zoneId = region.getString("ZoneId");
-                result.add(new DataCenter(zoneId, zoneId, providerRegionId, true, true));
+        String regionId = context.getRegionId();
+        if (regionId == null) {
+            throw new InternalException("No region is established for this request");
+        }
+
+        for (DataCenter dataCenter : listDataCenters(regionId)) {
+            if (dataCenter.getProviderDataCenterId().equals(providerDataCenterId)) {
+                return dataCenter;
             }
-            return result;
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Region getRegion(@Nonnull String providerRegionId) throws InternalException, CloudException {
+        for (Region region : listRegions()) {
+            if (region.getProviderRegionId().equals(providerRegionId)) {
+                return region;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public @Nonnull Iterable<DataCenter> listDataCenters(@Nonnull String providerRegionId) throws InternalException, CloudException {
+        APITrace.begin(getProvider(), "listDataCenters");
+        try {
+            ProviderContext context = getProvider().getContext();
+            if (context == null) {
+                throw new InternalException("No context was set for this request");
+            }
+
+            Cache<DataCenter> cache = null;
+            Collection<DataCenter> dataCenters;
+
+            if (providerRegionId.equals(context.getRegionId())) {//only cache context's region
+                cache = Cache.getInstance(getProvider(), "dataCenters", DataCenter.class, CacheLevel.REGION_ACCOUNT);
+                dataCenters = (Collection<DataCenter>) cache.get(context);
+                if (dataCenters != null) {
+                    return dataCenters;
+                }
+            }
+            dataCenters = new ArrayList<DataCenter>();
+
+            Map<String, Object> parameters = new HashMap<String, Object>();
+            parameters.put("RegionId", providerRegionId);
+            AliyunMethod method = new AliyunMethod(getProvider(), AliyunMethod.Category.ECS, "DescribeZones",
+                    parameters);
+
+            JSONObject json = method.get().asJson();
+
+            JSONArray dataCentersJson = json.getJSONObject("Zones").getJSONArray("Zone");
+            for (int i = 0; i < dataCentersJson.length(); i++) {
+                JSONObject dataCenterJson = dataCentersJson.getJSONObject(i);
+                String zoneId = dataCenterJson.getString("ZoneId");
+                DataCenter dataCenter = new DataCenter(zoneId, zoneId, providerRegionId, true, true);
+                JSONArray resourceTypesJson = dataCenterJson.getJSONObject("AvailableResourceCreation").getJSONArray("ResourceTypes");
+                List<String> resourceTypes = new ArrayList<String>();
+                for (int j = 0; j < resourceTypesJson.length(); j++) {
+                    resourceTypes.add(resourceTypesJson.getString(j));
+                }
+                dataCenter.setCompute(resourceTypes.contains("Instance"));
+                dataCenter.setStorage(resourceTypes.contains("Disk"));
+                dataCenters.add(dataCenter);
+            }
+
+            if (cache != null) {
+                cache.put(context, dataCenters);
+            }
+            return dataCenters;
         } catch (JSONException jsonException) {
             stdLogger.error("Failed to parse JSON due to field not exist", jsonException);
             throw new InternalException(jsonException);
+        } finally {
+            APITrace.end();
         }
     }
 
     @Override
     public @Nonnull Iterable<Region> listRegions() throws InternalException, CloudException {
-        AliyunMethod method = new AliyunMethod(getProvider(), AliyunMethod.Category.ECS, "DescribeRegions");
-        JSONObject json = method.get().asJson();
-        List<Region> result = new ArrayList<Region>();
+        APITrace.begin(getProvider(), "listRegions");
+
         try {
-            JSONArray regions = json.getJSONObject("Regions").getJSONArray("Region");
-            for (int i = 0; i < regions.length(); i++) {
-                JSONObject region = regions.getJSONObject(i);
-                String regionId = region.getString("RegionId");
-                result.add(new Region(regionId, regionId, true, true));
+            ProviderContext context = getProvider().getContext();
+            if (context == null) {
+                throw new InternalException("No context was set for this request");
             }
-            return result;
-        } catch (JSONException jsonException) {
+
+            Cache<Region> cache = Cache.getInstance(getProvider(), "regions", Region.class, CacheLevel.CLOUD_ACCOUNT);
+            Collection<Region> regions = (Collection<Region>) cache.get(context);
+            if (regions != null) {
+                return regions;
+            }
+            regions = new ArrayList<Region>();
+
+            AliyunMethod method = new AliyunMethod(getProvider(), AliyunMethod.Category.ECS, "DescribeRegions");
+            JSONObject json = method.get().asJson();
+
+            JSONArray regionsJson = json.getJSONObject("Regions").getJSONArray("Region");
+            for (int i = 0; i < regionsJson.length(); i++) {
+                JSONObject regionJson = regionsJson.getJSONObject(i);
+                String regionId = regionJson.getString("RegionId");
+                Region region = new Region(regionId, regionId, true, true);
+                if (regionId.equals("cn-hongkong")) {
+                    region.setJurisdiction("HK");
+                } else {
+                    region.setJurisdiction(regionId.substring(0, regionId.indexOf('-')).toUpperCase(Locale.ENGLISH));
+                }
+                regions.add(region);
+            }
+
+            cache.put(context, regions);
+            return regions;
+        }  catch (JSONException jsonException) {
             stdLogger.error("Failed to parse JSON due to field not exist", jsonException);
             throw new InternalException(jsonException);
+        } finally {
+            APITrace.end();
         }
     }
 }
--- src/main/java/org/dasein/cloud/aliyun/AliyunMethod.java
+++ src/main/java/org/dasein/cloud/aliyun/AliyunMethod.java
@@ -34,12 +34,17 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.HttpStatus;
 import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.GzipDecompressingEntity;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
@@ -62,7 +67,15 @@ import java.net.URLEncoder;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.SimpleTimeZone;
+import java.util.UUID;
 
 /**
  * Created by Jeffrey Yan on 5/5/2015.
@@ -108,11 +121,12 @@ public class AliyunMethod {
         try {
             return URLEncoder.encode(value, "UTF-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~");
         } catch (UnsupportedEncodingException unsupportedEncodingException) {
+            stdLogger.error("AliyunMethod.urlEncode() failed due to encoding not supported: " + unsupportedEncodingException.getMessage());
             throw new InternalException(unsupportedEncodingException);
         }
     }
 
-    private String sign(byte[] accessKeySecret, Map<String, String> requestParameters) throws InternalException {
+    private String sign(String httpMethod, Map<String, String> requestParameters) throws InternalException {
         String signature;
         String[] sortedKeys = requestParameters.keySet().toArray(new String[]{});
         Arrays.sort(sortedKeys);
@@ -124,9 +138,11 @@ public class AliyunMethod {
         String canonicalString = canonicalStringBuilder.toString().substring(1);
 
         StringBuilder stringToSign = new StringBuilder();
-        stringToSign.append("GET").append("&").append(urlEncode("/")).append("&");
+        stringToSign.append(httpMethod).append("&").append(urlEncode("/")).append("&");
         stringToSign.append(urlEncode(canonicalString));
 
+        byte[][] accessKey = ( byte[][] ) aliyun.getContext().getConfigurationValue(Aliyun.DSN_ACCESS_KEY);
+        byte[] accessKeySecret = accessKey[1];
         try {
             Mac mac = Mac.getInstance("HmacSHA1");
             byte[] secretKey = Arrays.copyOf(accessKeySecret, accessKeySecret.length + 1);
@@ -135,13 +151,113 @@ public class AliyunMethod {
             byte[] signedData = mac.doFinal(stringToSign.toString().getBytes("UTF-8"));
             signature = new String(Base64.encodeBase64(signedData));
         } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            stdLogger.error("AliyunMethod.sign() failed due to algorithm not supported: " + noSuchAlgorithmException.getMessage());
             throw new InternalException(noSuchAlgorithmException);
         } catch (InvalidKeyException invalidKeyException) {
+            stdLogger.error("AliyunMethod.sign() failed due to key invalid: " + invalidKeyException.getMessage());
             throw new InternalException(invalidKeyException);
         } catch (UnsupportedEncodingException unsupportedEncodingException) {
+            stdLogger.error("AliyunMethod.sign() failed due to encoding not supported: " + unsupportedEncodingException.getMessage());
             throw new InternalException(unsupportedEncodingException);
         }
         return signature;
+    }
+
+    private  Map<String, String> transformParameters(String httpMethod) throws InternalException {
+        byte[][] accessKey = ( byte[][] ) aliyun.getContext().getConfigurationValue(Aliyun.DSN_ACCESS_KEY);
+
+        Map<String, String> requestParameters = new HashMap<String, String>();
+        requestParameters.put("Action", this.action);
+        requestParameters.put("Version", "2014-05-26");
+        requestParameters.put("AccessKeyId", new String(accessKey[0]));
+        requestParameters.put("TimeStamp", formatIso8601Date(new Date()));
+        requestParameters.put("SignatureMethod", "HMAC-SHA1");
+        requestParameters.put("SignatureVersion", "1");
+        requestParameters.put("SignatureNonce", UUID.randomUUID().toString());
+        requestParameters.put("Format", "JSON");
+        for (Map.Entry<String, Object> parameter : this.parameters.entrySet()) {
+            Object value = parameter.getValue();
+            if (value instanceof Date) {
+                requestParameters.put(parameter.getKey(), formatIso8601Date((Date) value));
+            } else {
+                requestParameters.put(parameter.getKey(), value.toString());
+            }
+        }
+
+        String signature = sign(httpMethod, requestParameters);
+        requestParameters.put("Signature", signature);
+
+        return requestParameters;
+    }
+
+    private Response invoke(HttpRequestBase httpRequest) throws InternalException, CloudException {
+        if( wireLogger.isDebugEnabled() ) {
+            wireLogger.debug(httpRequest.getRequestLine().toString());
+            for( Header header : httpRequest.getAllHeaders() ) {
+                wireLogger.debug(header.getName() + ": " + header.getValue());
+            }
+            wireLogger.debug("");
+        }
+
+        HttpResponse httpResponse;
+        try {
+            HttpClient client = getClient();
+            httpResponse = client.execute(httpRequest);
+            if( wireLogger.isDebugEnabled() ) {
+                wireLogger.debug(httpResponse.getStatusLine().toString());
+                for( Header header : httpResponse.getAllHeaders() ) {
+                    wireLogger.debug(header.getName() + ": " + header.getValue());
+                }
+                wireLogger.debug("");
+            }
+        } catch( IOException ioException ) {
+            stdLogger.error("I/O error from server communications: " + ioException.getMessage());
+            throw new InternalException(ioException);
+        }
+        int httpCode = httpResponse.getStatusLine().getStatusCode();
+
+        stdLogger.debug("HTTP STATUS: " + httpCode);
+
+        if( httpCode == HttpStatus.SC_NOT_FOUND || httpCode == HttpStatus.SC_GONE ) {
+            return null;
+        }
+        if (httpCode < HttpStatus.SC_OK || httpCode >= HttpStatus.SC_MULTIPLE_CHOICES) {
+            stdLogger.error("Unexpected OK for GET request, got " + httpCode);
+
+            HttpEntity entity = httpResponse.getEntity();
+            if (entity == null) {
+                throw new CloudException();
+            }
+            Response response = new Response(entity);
+            JSONObject json = response.asJson();
+            if( wireLogger.isDebugEnabled() ) {
+                wireLogger.debug(json);
+                wireLogger.debug("");
+            }
+            String code;
+            String message;
+            String requestId;
+            String hostId;
+            try {
+                code = json.getString("Code");
+                message = json.getString("Message");
+                requestId = json.getString("RequestId");
+                hostId = json.getString("HostId");
+            } catch (JSONException jsonException) {
+                stdLogger.error("Failed to parse JSON due to field not exist", jsonException);
+                throw new InternalException(jsonException);
+            }
+            stdLogger.error(
+                    " [" + "code:" + code + "; message:" + message + "; requestId:" + requestId + "; hostId:"
+                            + hostId + "] ");
+            throw AliyunException.newInstance(httpCode, code, message, requestId, hostId);
+        } else {
+            HttpEntity entity = httpResponse.getEntity();
+            if (entity == null) {
+                throw new CloudException();
+            }
+            return new Response(entity);
+        }
     }
 
     public Response get() throws InternalException, CloudException {
@@ -153,112 +269,22 @@ public class AliyunMethod {
             wireLogger.debug(">>> [GET (" + (new Date()) + ")] -> " + category.getHost()
                     + " >--------------------------------------------------------------------------------------");
         }
-
         try {
+            Map<String, String> requestParameters = transformParameters("GET");
 
-            byte[][] accessKey = ( byte[][] ) aliyun.getContext().getConfigurationValue(Aliyun.DSN_ACCESS_KEY);
-
-            Map<String, String> requestParameters = new HashMap<String, String>();
-            requestParameters.put("Action", this.action);
-            requestParameters.put("Version", "2014-05-26");
-            requestParameters.put("AccessKeyId", new String(accessKey[0]));
-            requestParameters.put("TimeStamp", formatIso8601Date(new Date()));
-            requestParameters.put("SignatureMethod", "HMAC-SHA1");
-            requestParameters.put("SignatureVersion", "1");
-            requestParameters.put("SignatureNonce", UUID.randomUUID().toString());
-            requestParameters.put("Format", "JSON");
-            for (Map.Entry<String, Object> parameter : this.parameters.entrySet()) {
-                Object value = parameter.getValue();
-                if (value instanceof Date) {
-                    requestParameters.put(parameter.getKey(), formatIso8601Date((Date) value));
-                } else {
-                    requestParameters.put(parameter.getKey(), value.toString());
-                }
-            }
-
-            String signature = sign(accessKey[1], requestParameters);
-            requestParameters.put("Signature", signature);
-
-            HttpGet httpGet;
+            HttpGet httpGet = new HttpGet();
             URIBuilder uriBuilder = new URIBuilder();
             uriBuilder.setScheme("https").setHost(category.getHost()).setPath("/");
             for(Map.Entry<String, String> requestParameter: requestParameters.entrySet()) {
                 uriBuilder.setParameter(requestParameter.getKey(), requestParameter.getValue());
             }
             try {
-                httpGet = new HttpGet(uriBuilder.build());
+                httpGet.setURI(uriBuilder.build());
             } catch (URISyntaxException uriSyntaxException) {
+                stdLogger.error("AliyunMethod.get() failed due to URI invalid: " + uriSyntaxException.getMessage());
                 throw new InternalException(uriSyntaxException);
             }
-
-            if( wireLogger.isDebugEnabled() ) {
-                wireLogger.debug(httpGet.getRequestLine().toString());
-                for( Header header : httpGet.getAllHeaders() ) {
-                    wireLogger.debug(header.getName() + ": " + header.getValue());
-                }
-                wireLogger.debug("");
-            }
-
-            HttpResponse httpResponse;
-
-            try {
-                HttpClient client = getClient();
-                httpResponse = client.execute(httpGet);
-                if( wireLogger.isDebugEnabled() ) {
-                    wireLogger.debug(httpResponse.getStatusLine().toString());
-                    for( Header header : httpResponse.getAllHeaders() ) {
-                        wireLogger.debug(header.getName() + ": " + header.getValue());
-                    }
-                    wireLogger.debug("");
-                }
-            } catch( IOException ioException ) {
-                stdLogger.error("I/O error from server communications: " + ioException.getMessage());
-                throw new InternalException(ioException);
-            }
-            int httpCode = httpResponse.getStatusLine().getStatusCode();
-
-            stdLogger.debug("HTTP STATUS: " + httpCode);
-
-            if( httpCode == HttpStatus.SC_NOT_FOUND || httpCode == HttpStatus.SC_GONE ) {
-                return null;
-            }
-            if (httpCode < HttpStatus.SC_OK || httpCode >= HttpStatus.SC_MULTIPLE_CHOICES) {
-                stdLogger.error("Unexpected OK for GET request, got " + httpCode);
-
-                HttpEntity entity = httpResponse.getEntity();
-                if (entity == null) {
-                    throw new CloudException();
-                }
-                Response response = new Response(entity);
-                JSONObject json = response.asJson();
-                if( wireLogger.isDebugEnabled() ) {
-                    wireLogger.debug(json);
-                    wireLogger.debug("");
-                }
-                String code;
-                String message;
-                String requestId;
-                String hostId;
-                try {
-                    code = json.getString("Code");
-                    message = json.getString("Message");
-                    requestId = json.getString("RequestId");
-                    hostId = json.getString("HostId");
-                } catch (JSONException jsonException) {
-                    stdLogger.error("Failed to parse JSON due to field not exist", jsonException);
-                    throw new InternalException(jsonException);
-                }
-                stdLogger.error(
-                        " [" + "code:" + code + "; message:" + message + "; requestId:" + requestId + "; hostId:"
-                                + hostId + "] ");
-                throw AliyunException.newInstance(httpCode, code, message, requestId, hostId);
-            } else {
-                HttpEntity entity = httpResponse.getEntity();
-                if (entity == null) {
-                    throw new CloudException();
-                }
-                return new Response(entity);
-            }
+            return invoke(httpGet);
         } finally {
             if( stdLogger.isTraceEnabled() ) {
                 stdLogger.trace("EXIT - " + AliyunMethod.class.getName() + ".get(" + category.getHost() + ")");
@@ -271,8 +297,49 @@ public class AliyunMethod {
         }
     }
 
-    public Response post() {
-        return null;
+    public Response post() throws InternalException, CloudException {
+        if( stdLogger.isTraceEnabled() ) {
+            stdLogger.trace("ENTER - " + AliyunMethod.class.getName() + ".post(" + category.getHost()  + ")");
+        }
+        if( wireLogger.isDebugEnabled() ) {
+            wireLogger.debug("");
+            wireLogger.debug(">>> [POST (" + (new Date()) + ")] -> " + category.getHost()
+                    + " >--------------------------------------------------------------------------------------");
+        }
+        try {
+            Map<String, String> requestParameters = transformParameters("POST");
+
+            HttpPost httpPost = new HttpPost();
+            URIBuilder uriBuilder = new URIBuilder();
+            uriBuilder.setScheme("https").setHost(category.getHost()).setPath("/");
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+            for( Map.Entry<String, String> entry : requestParameters.entrySet() ) {
+                params.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+
+            try {
+                httpPost.setURI(uriBuilder.build());
+                httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+            } catch (URISyntaxException uriSyntaxException) {
+                stdLogger.error("AliyunMethod.post() failed due to URI invalid: " + uriSyntaxException.getMessage());
+                throw new InternalException(uriSyntaxException);
+            } catch (UnsupportedEncodingException unsupportedEncodingException) {
+                stdLogger.error("AliyunMethod.post() failed due to encoding not supported: " + unsupportedEncodingException.getMessage());
+                throw new InternalException(unsupportedEncodingException);
+            }
+            return invoke(httpPost);
+        } finally {
+            if( stdLogger.isTraceEnabled() ) {
+                stdLogger.trace("EXIT - " + AliyunMethod.class.getName() + ".post(" + category.getHost() + ")");
+            }
+            if( wireLogger.isDebugEnabled() ) {
+                wireLogger.debug(">>> [POST (" + (new Date()) + ")] -> " + category.getHost()
+                        + " >--------------------------------------------------------------------------------------");
+                wireLogger.debug("");
+            }
+        }
     }
 
     private HttpClient getClient() throws InternalException {
@@ -297,8 +364,7 @@ public class AliyunMethod {
             }
             if( proxyHost != null && proxyHost.length() > 0 && proxyPort > 0 ) {
                 params.setParameter(ConnRoutePNames.DEFAULT_PROXY,
-                        new HttpHost(proxyHost, proxyPort)
-                );
+                        new HttpHost(proxyHost, proxyPort));
             }
         }
         DefaultHttpClient client = new DefaultHttpClient(params);
