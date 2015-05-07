86e33f5ecef38d48bd7f36b57cdaafe4f976800b
JanewzhWang <Jane.Wang@software.dell.com>
2015-05-07 16:45:42 +0800

    add copyright and change supportsVLANAddress to false, target can only be a instance

--- src/main/java/org/dasein/cloud/aliyun/network/AliyunIpAddressCapabilities.java
+++ src/main/java/org/dasein/cloud/aliyun/network/AliyunIpAddressCapabilities.java
@@ -1,6 +1,25 @@
+/*
+ * *
+ *  * Copyright (C) 2009-2015 Dell, Inc.
+ *  * See annotations for authorship information
+ *  *
+ *  * ====================================================================
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *  * ====================================================================
+ *
+ */
 package org.dasein.cloud.aliyun.network;
 
-import org.bouncycastle.util.IPAddress;
 import org.dasein.cloud.AbstractCapabilities;
 import org.dasein.cloud.CloudException;
 import org.dasein.cloud.InternalException;
@@ -9,14 +28,15 @@ import org.dasein.cloud.aliyun.Aliyun;
 import org.dasein.cloud.compute.VmState;
 import org.dasein.cloud.network.IPAddressCapabilities;
 import org.dasein.cloud.network.IPVersion;
-import org.dasein.cloud.util.requester.fluent.Requester;
-
 import javax.annotation.Nonnull;
 import java.util.Collections;
 import java.util.Locale;
 
 /**
- * Created by jwang7 on 5/7/2015.
+ * Created by Jane Wang on 5/7/2015.
+ *
+ * @author Jane Wang
+ * @since 2015.5.7
  */
 public class AliyunIpAddressCapabilities extends AbstractCapabilities<Aliyun> implements IPAddressCapabilities {
 
@@ -70,6 +90,6 @@ public class AliyunIpAddressCapabilities extends AbstractCapabilities<Aliyun> im
     }
 
     public boolean supportsVLANAddresses(@Nonnull IPVersion ofVersion) throws InternalException, CloudException {
-        return true; //TODO VPC Platform
+        return false;
     }
 }
